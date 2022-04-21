403b856b85904ac3ec967de1cd9267458f7e538b
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-04-21 18:06:16 +0900

    API

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,31 @@ public class HelloController {
         model.addAttribute("name",name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 body에 직접 넣어주겠다. html 코드가 그냥 나옴
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello { // static을 사용하면 class 내에서 class 사용 가능
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
